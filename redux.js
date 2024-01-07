e13dd54b0eb034a5715014e9e2bca20921a24181
mayankvaidya436 <mayankvaidya436@gmail.com>
2024-01-07 14:06:07 +0530

    Sharpener-tech-Assignment-Redux

---
+++ redux.js
@@ -0,0 +1,31 @@
+const redux=require('redux')
+
+const counterReducer=(state={counter:0},action)=>{
+   if(action.type==='increment')
+   {
+    return { 
+
+        counter:state.counter+1
+    }
+   }
+    if(action.type==='decrement')
+    {
+       return {
+        counter:state.counter-1
+       }
+    }
+}
+
+const store=redux.createStore(counterReducer);
+  
+const counterSubsribe=()=>{
+    const latestState=store.getState();
+    console.log(latestState)
+}
+
+store.subscribe(counterSubsribe)
+for(let i=0;i<5;i++)
+{
+store.dispatch({type:'increment'})
+}
+store.dispatch({type:'decrement'})
